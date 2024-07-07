ea3f43b1d3f8f637144af1a9b88f2c3251969401
unknown <muhammadabubakarjamiu@gmail.com>
2024-07-07 02:50:43 -0700

    recomitting

--- src/assets/newsLetter.jsx
+++ src/assets/newsLetter.jsx
@@ -5,7 +5,7 @@ function NewsLetter() {
   return (
     <div className=' letter'>
         <h1 className=''>NEWS LETTER</h1>
-        <h2>Always in touch with us for your daily news</h2>
+        <h2>Always in touch with Accidic news for your daily news</h2>
       <div className="form d-flex">
         <input type="email"  placeholder='enter your email'/>
         <button>send</button>
